6ba477720c5d0a1324f8302462bd30b38fdda894
gabrielwoolls <gabrielwoolls@gmail.com>
2023-04-30 00:46:17 -0700

    unknown changes, committed 2 years later

---
+++ optimization/j_opt_peps.py
@@ -0,0 +1,76 @@
+import autoray
+
+
+def tf_qr(x):
+    U, s, VH = autoray.do('linalg.svd', x)
+    
+    dtype = autoray.get_dtype_name(U)
+    if 'complex' in dtype:
+        s = autoray.astype(s, dtype)
+    
+    Q = U
+    R = autoray.reshape(s, (-1, 1)) * VH
+    
+    return Q, R
+
+
+autoray.register_function('tensorflow', 'linalg.qr', tf_qr)
+
+import quimb as qu
+import quimb.tensor as qtn
+from quimb.tensor.optimize import TNOptimizer
+
+LX, LY = 2, 2
+DTYPE = 'complex128'
+
+
+autodiff_backend = 'tensorflow'
+autodiff_backend_opts = {'experimental_compile': True}
+
+
+def state_energy(psi, hterms, vterms, **opts):
+    
+    he = psi.compute_local_expectation(
+        hterms, normalized=True, **opts)
+
+    ve = psi.compute_local_expectation(
+        vterms, normalized=True, **opts)        
+    
+    return autoray.do('real', (he + ve))
+
+
+Hij = qu.ham_heis(2).astype(DTYPE)
+
+peps = qtn.PEPS.rand(LX, LY, bond_dim=2, dtype=DTYPE)
+
+hterms = {coos: Hij for coos in peps.gen_horizontal_bond_coos()}
+vterms = {coos: Hij for coos in peps.gen_vertical_bond_coos()}
+
+compute_expec_opts = dict(
+                cutoff=2e-3, 
+                max_bond=9, 
+                contract_optimize='random-greedy')
+
+Hij = qu.ham_heis(2).astype(DTYPE)
+
+peps = qtn.PEPS.rand(LX, LY, bond_dim=2, dtype=DTYPE)
+
+hterms = {coos: Hij for coos in peps.gen_horizontal_bond_coos()}
+vterms = {coos: Hij for coos in peps.gen_vertical_bond_coos()}
+
+compute_expec_opts = dict(
+                cutoff=0.0, 
+                max_bond=9, 
+                contract_optimize='random-greedy')
+
+optmzr = TNOptimizer(
+    peps, 
+    loss_fn=state_energy,
+    loss_constants={'hterms': hterms,
+                    'vterms': vterms},
+    loss_kwargs=compute_expec_opts,  # these weren't being supplied quite right
+    autodiff_backend=autodiff_backend,
+    **autodiff_backend_opts,
+)
+
+peps_opt = optmzr.optimize(1000)
